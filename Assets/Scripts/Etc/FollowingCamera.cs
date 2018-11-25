a919280221be060483de1c97eb52d416b931d66b
Simple <simple3068@naver.com>
2018-11-26 00:23:55 +0900

    Init

---
+++ Assets/Scripts/Etc/FollowingCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour
+{
+    [SerializeField] float fFollowingSpeed;
+    [SerializeField] Transform trsTarget;
+
+    Vector3 v3NewPosition;
+
+	void Update ()
+    {
+        v3NewPosition = trsTarget.position;
+        v3NewPosition.z = transform.position.z;
+
+        transform.position = Vector3.Slerp(transform.position, v3NewPosition, fFollowingSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Player/HelicopterController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelicopterController : MonoBehaviour
+{
+    [Header("Helicopter Info")]
+    [SerializeField] Transform trsPlayer;
+    [SerializeField] Transform trsCenterOfMass;
+    [SerializeField] Rigidbody2D rigidBody;
+    [Space]
+    [SerializeField] Transform trsMainBlade;
+    [SerializeField] float fMainRotorSpeed;
+    [SerializeField] Transform trsTailBlade;
+    [SerializeField] float fTailRotorSpeed;
+    [Space]
+    [SerializeField] float fEngineStartingTime;
+    [SerializeField] float fEngineStoppingTime;
+
+    const float N_ENGINE_RUNNING_STEPS = 32;
+    const float F_ENGINE_RUNNING_DELTA = 1.0f / N_ENGINE_RUNNING_STEPS;
+
+    float fThrottle;
+    float fCollective;
+    float fCycle;
+
+    bool bEngineStatus = false;
+    bool bEngineRunning = false;
+    bool bAvailableFlight = true;
+
+    void Start()
+    {
+        rigidBody.centerOfMass = trsCenterOfMass.localPosition;
+    }
+
+    void FixedUpdate()
+    {
+        if (bEngineStatus)
+        {
+            rigidBody.AddForce(trsPlayer.up * Physics2D.gravity.magnitude * fThrottle);
+        }
+    }
+
+    void Update()
+    {
+        if (bAvailableFlight && !bEngineRunning && Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (!bEngineStatus)
+            {
+                StartCoroutine("StartEngine");
+            }
+            else
+            {
+                StartCoroutine("StopEngine");
+            }
+        }
+
+        // TODO: Movable Controllable
+
+        BladeRotation();
+    }
+
+    IEnumerator StartEngine()
+    {
+        bEngineRunning = true;
+
+        while (fThrottle < 1.0f)
+        {
+            fThrottle += F_ENGINE_RUNNING_DELTA;
+            Debug.Log("Engine is starting, throttle: " + fThrottle);
+            yield return new WaitForSeconds(fEngineStartingTime / N_ENGINE_RUNNING_STEPS);
+        }
+
+        bEngineStatus = true;
+        bEngineRunning = false;
+    }
+    IEnumerator StopEngine()
+    {
+        bEngineRunning = true;
+
+        while (fThrottle > 0.0f)
+        {
+            fThrottle -= F_ENGINE_RUNNING_DELTA;
+            Debug.Log("Engine is stopping, throttle: " + fThrottle);
+            yield return new WaitForSeconds(fEngineStoppingTime / N_ENGINE_RUNNING_STEPS);
+        }
+
+        bEngineStatus = false;
+        bEngineRunning = false;
+    }
+
+    void BladeRotation()
+    {
+        trsMainBlade.Rotate(0.0f, fThrottle * fMainRotorSpeed, 0.0f);
+        trsTailBlade.Rotate(0.0f, 0.0f, -fThrottle * fTailRotorSpeed);
+    }
+}
